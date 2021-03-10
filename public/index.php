759d67a3c722299872d7decf1e8a754af001b72a
nellinger <nellinger@vonaffenfels.de>
2021-03-10 19:14:40 +0100

    Add Dependency Injection

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
 
+use Commands\errors\InOutException;
 use Contentus\Application;
+use DI\Container;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Application();
-$app->run();
+$diContainer = new Container();
 
+try {
+    $app = $diContainer->get(Application::class);
+    $app->run();
+}
+catch (InOutException $exception)
+{
+    echo $exception->getMessage();
+}
 
 echo "<br><br><a href=/contact>Contact</a>";
 echo "<br><a href=/about>About</a>";
